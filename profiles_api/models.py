83a685f9cb6ad5a5db075c845f2083a7eeaf9bfb
irwanto.cia <irwanto.cia@mcpayment.co.id>
2020-10-29 17:50:23 +0700

    Add feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
 # Create your models here.
@@ -21,8 +22,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-
-    def create_superuser(self,email, name, password):
+    def create_superuser(self, email, name, password):
         """Create new superuser"""
         user = self.create_user(email, name, password)
 
@@ -56,3 +56,17 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return user string representation"""
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """return the model as a string"""
+        return self.status_text
